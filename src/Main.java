238228b5d1f6e7014b7539b98b8ec11cba5ba53a
SchatzerZD <daniel.walaker@gmail.com>
2022-03-09 16:52:26 +0100

    Imported javafx

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
