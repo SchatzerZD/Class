de0dc2cde779d13f5d30b8f2f82a71438632224b
SchatzerZD <daniel.walaker@gmail.com>
2022-03-11 13:33:47 +0100

    11/03/2022 CSS with JavaFX

---
+++ Daniel/MavenGUI/src/main/java/com/test/mavengui/LoginMenu.java
@@ -0,0 +1,11 @@
+package com.test.mavengui;
+
+public class LoginMenu {
+
+
+
+    public LoginMenu(){
+
+    }
+
+}
--- Daniel/MavenGUI/src/main/java/com/test/mavengui/TournamentApplicationController.java
+++ Daniel/MavenGUI/src/main/java/com/test/mavengui/TournamentApplicationController.java
@@ -3,7 +3,7 @@ package com.test.mavengui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TournamentApplicationController {
     @FXML
     private Label welcomeText;
 
